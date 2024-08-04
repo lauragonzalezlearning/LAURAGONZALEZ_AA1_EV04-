cf239ba312473a382f4be9f132f9a984c480d68c
laurago <laugostar@outlook.com>
2024-08-03 21:55:26 -0500

    estos son nuevos comandos

--- git.php
+++ git.php
@@ -5,4 +5,7 @@ echo "Git init : permite inicializar "."</br>";
 echo "Git add : permite iniciarlizar "."</br>";
 echo "Git log muestra el historial de los commities"."</br>";
 
+echo "Git log muestra el historial de los commities"."</br>";
+echo "Git branch -m : "."</br>;";
+
 ?>
